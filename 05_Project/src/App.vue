6af469c49513bb48697e4c5cedec9cbc34527e0f
Sourav Samanta <souravthewriter95@gmail.com>
2025-05-14 11:53:49 +0530

    component basic

---
+++ 05_Project/src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import task from "@/components/task.vue";
+export default {
+	components: {
+		task,
+	},
+	data() {
+		return {
+			titleVar: "Learn Vue3",
+			taskStatus: false,
+		};
+	},
+	methods: {
+		updateTaskStatus() {
+			this.taskStatus = !this.taskStatus;
+		},
+	},
+	computed: {},
+};
+</script>
+
+<template>
+	<h2>Learn about components</h2>
+	<pre>{{ taskStatus }}</pre>
+	<task
+		:title="titleVar"
+		desc="All theories"
+		:isDone="taskStatus"
+		@changeTaskStatus="updateTaskStatus"
+	></task>
+	<task title="Learn CSS" desc="All properties"></task>
+</template>
+
+<style scoped></style>
---
+++ 05_Project/src/components/task.vue
@@ -0,0 +1,30 @@
+<template>
+	<h4>{{ title }}</h4>
+	<p>{{ desc }}</p>
+	<input type="checkbox" :value="isDone" @input="onInput" />
+</template>
+
+<script>
+export default {
+	props: {
+		title: {
+			type: [String, Number, Object, null],
+			default: "test",
+		},
+		desc: String,
+		isDone: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	emits: ["changeTaskStatus"],
+	methods: {
+		onInput() {
+			this.$emit("changeTaskStatus");
+			// Custom event
+		},
+	},
+};
+</script>
+
+<style scoped></style>
