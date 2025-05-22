d378f0d4746abefb6aa4dbab1ee3c171c63220e3
Sourav Samanta <souravthewriter95@gmail.com>
2025-05-22 08:52:50 +0530

    assignment project

---
+++ 08_Project/src/App.vue
@@ -0,0 +1,23 @@
+<script></script>
+
+<template>
+	<h2>Assignment Project</h2>
+	<h3>App Name: Student & Teacher</h3>
+	<h4>
+		An Application where student can send course request to teacher by sending
+		name, email and subject, teacher name(select)
+	</h4>
+	<h4>
+		In the right side there will be a list three teachers including their name,
+		years of experience(in year) and subject, should also include application
+		count and details button. By clicking on details button we can see the how
+		many application recived and their details. Teacher can accpet or reject the
+		application.
+	</h4>
+	<h4>
+		left side bellow, we can see all application list with details, the
+		application status should also update here
+	</h4>
+</template>
+
+<style scoped></style>
